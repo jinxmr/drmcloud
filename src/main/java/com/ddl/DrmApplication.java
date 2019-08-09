52c385ef7c730e653e54aa27d0264e8b196ced86
jxmsir <jinxmsir@163.com>
2019-08-09 16:46:56 +0800

    时区修改

--- src/main/java/com/ddl/DrmApplication.java
+++ src/main/java/com/ddl/DrmApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @MapperScan("com.ddl.web.system.*.mapper")
 public class DrmApplication {
@@ -12,4 +15,8 @@ public class DrmApplication {
 		SpringApplication.run(DrmApplication.class, args);
 	}
 
+	@PostConstruct
+	void setDefaultTimezone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+	}
 }
