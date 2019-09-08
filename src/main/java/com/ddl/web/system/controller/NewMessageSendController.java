d75523a6cd2f56d3cbfba85dda67371fdf3f1644
jinxmsir@163.cm <jinxmsir@163.cm>
2019-09-08 21:34:54 +0800

    继承websocket

---
+++ src/main/java/com/ddl/web/system/controller/NewMessageSendController.java
@@ -0,0 +1,36 @@
+package com.ddl.web.system.controller;
+
+import com.ddl.config.socket.WebSocketServer;
+import com.ddl.model.AjaxResult;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+
+@Controller
+@RequestMapping("back/socket")
+public class NewMessageSendController {
+    //页面请求
+    @GetMapping("ask/{cid}")
+    public ModelAndView socket(@PathVariable String cid) {
+        ModelAndView mav=new ModelAndView("/socket");
+        mav.addObject("cid", cid);
+        return mav;
+    }
+    //推送数据接口
+    @ResponseBody
+    @RequestMapping("push/{cid}")
+    public AjaxResult pushToWeb(@PathVariable String cid, String message) {
+        try {
+            WebSocketServer.sendInfo(message,cid);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return AjaxResult.error(cid+"#"+e.getMessage());
+        }
+        return AjaxResult.success(cid);
+    }
+}
