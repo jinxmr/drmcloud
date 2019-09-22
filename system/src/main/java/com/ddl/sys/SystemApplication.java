b2a2103d79b783156b861a88b270fd8555688734
jinxmsir@163.cm <jinxmsir@163.cm>
2019-09-22 22:59:50 +0800

    test

--- system/src/main/java/com/ddl/sys/SystemApplication.java
+++ system/src/main/java/com/ddl/sys/SystemApplication.java
@@ -14,6 +14,7 @@ import java.util.TimeZone;
 public class SystemApplication {
 
     public static void main(String[] args) {
+        System.out.println("SpringCloud");
         SpringApplication.run(SystemApplication.class, args);
     }
 
