e52a0968f83f7c3d0205e8e36006082834c46dd3
jinxmsir@163.cm <jinxmsir@163.cm>
2019-11-16 22:58:15 +0800

    集成rabbitMQ

--- drm-rabbitmq/src/main/java/com/ddl/rabbitmq/message/MessageReceive.java
+++ drm-rabbitmq/src/main/java/com/ddl/rabbitmq/message/MessageReceive.java
@@ -1,6 +1,7 @@
 package com.ddl.rabbitmq.message;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 
